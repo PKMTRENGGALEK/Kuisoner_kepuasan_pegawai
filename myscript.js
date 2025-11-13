a4ece7ad4a98ed8a08ed2fe4709cf3445b1b4fac
PKMTRENGGALEK <pkmpkmtrenggalek@gmail.com>
2025-11-13 13:01:00 +0700

    update

--- myscript.js
+++ myscript.js
@@ -1,41 +1,46 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
-  
-        form.classList.add('was-validated')
-      }, false)
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyCx5HWEHWcDV4NQ3lLfFhhrAl_29lGIt9Whn-BH7MafNrzpDefhOtGTat0VyH3YLPVJQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnLoading.classList.toggle("d-none");
+      btnKirim.classList.toggle("d-none");
+      form.reset();
+      window.location = "success.html";
+      console.log("Success!", response);
     })
-  })()
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbyCx5HWEHWcDV4NQ3lLfFhhrAl_29lGIt9Whn-BH7MafNrzpDefhOtGTat0VyH3YLPVJQ/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const btnKirim=document.querySelector('.btn-kirim');
-  const btnLoading=document.querySelector('.btn-loading');
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    btnLoading.classList.toggle('d-none');
-    btnKirim.classList.toggle('d-none');
-
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then((response) => {
-        btnLoading.classList.toggle('d-none');
-        btnKirim.classList.toggle('d-none');
-        form.reset();
-        window.location = "success.html";
-        console.log('Success!', response)
-      })
-      .catch((error) => console.error('Error!', error.message));
-  })
+    .catch((error) => console.error("Error!", error.message));
+});
